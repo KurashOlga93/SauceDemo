90f5e98fd6edffbb53b86ca2414215dcc0a8be0b
olgakurash <olga9793@gmail.com>
2025-02-05 21:08:48 +0300

    lesson18

---
+++ src/main/java/MainPage.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class MainPage {
+
+    WebDriver driver = new ChromeDriver();
+    final WebElement userName = driver.findElement(By.id("user-name"));
+    final WebElement password = driver.findElement(By.name("password"));
+    final WebElement loginButton = driver.findElement(By.className("submit-button"));
+    final WebElement listOfUsernames = driver.findElement(By.tagName("h4"));
+    final WebElement linkText = driver.findElement(By.linkText("All Items"));
+    final WebElement partialLinkText = driver.findElement(By.partialLinkText("All"));
+    final WebElement addToCard = driver.findElement(By.xpath("//*[@data-test='add-to-cart-sauce-labs-backpack']"));
+    final WebElement productName = driver.findElement(By.xpath("//*[@text()='Sauce Labs BackPack']"));
+    final WebElement productNamePart = driver.findElement(By.xpath("//*[contains(text(),'Light')"));
+    final List<WebElement> priceList = driver.findElements(By.xpath("//*[contains(@data-test,'price')"));
+    final WebElement descendant = driver.findElement(By.xpath("//*[@class='login_wrapper-inner']//descendant::div"));
+    final WebElement parent = driver.findElement(By.xpath("//*[@class='login_wrapper-inner']//parent::div"));
+    final WebElement preceding = driver.findElement(By.xpath("//*[@class='login_wrapper-inner']//parent::div"));
+    final WebElement precend1 = driver.findElement(By.xpath("//input[@id='password']/preceding::input[1]"));
+    final WebElement precend2 = driver.findElement(By.xpath("//input[@id='password']/preceding-sibling::input[1]"));
+    final WebElement precend3 = driver.findElement(By.xpath("//*[@class='login_wrapper-inner']/following-sibling::div"));
+    final WebElement andTest = driver.findElement(By.xpath("//*[@data-test='password' and contains(@id, 'password') and contains(@name, 'password')]"))
+    final WebElement byImage = driver.findElement(By.xpath("//img[@class=\"inventory_item_img\"]/@src"));
+    final WebElement byCss = driver.findElement(By.cssSelector("//img[@class=\"inventory_item_img\"]/@src"));
+
+}
