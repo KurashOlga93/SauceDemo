6e6d5c40006e7b6b0b4f5600394d17e679ccbf3b
olgakurash <olga9793@gmail.com>
2025-02-05 21:14:46 +0300

    lesson18

---
+++ src/test/java/tests/SauceDemoTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SauceDemoTest {
+
+    @Test
+    public void checkingProductInCart() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        driver.findElement(By.linkText("Sauce Labs Backpack")).click();
+        driver.findElement(By.name("add-to-cart")).click();
+        driver.findElement(By.xpath("//*[@data-test='shopping-cart-link']")).click();
+        String actualProductName = driver.findElement(By.xpath("//*[@data-test='inventory-item-name']")).getText();
+        Assert.assertEquals(actualProductName, "Sauce Labs Backpack");
+        String actualProductPrice = driver.findElement(By.xpath("//*[@data-test='inventory-item-price']")).getText();
+        Assert.assertEquals(actualProductPrice, "$29.99");
+    }
+}
